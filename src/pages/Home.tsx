bdde013ed167b0a1763e96fe4da6126c4d920229
“ARustamA” <rakhmetyanov@gmail.com>
2022-11-10 11:40:01 +0600

    2

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
+import { ReposCard } from '../components/ReposCard';
 import { useDebounce } from '../hooks/debounce';
-import { useSearchUsersQuery } from '../store/github/github.api'
+import { useSearchUsersQuery, useLazyGetUserReposQuery } from '../store/github/github.api'
 
 export function Home() {
    const [search, setSearch] = useState('');
@@ -8,13 +9,21 @@ export function Home() {
    const debounced = useDebounce(search);
    const { isLoading, isError, data } = useSearchUsersQuery(debounced, {
       skip: debounced.length < 3,
-      refetchOnFocus:true,
+      refetchOnFocus: true,
    });
+
+   const [fetchRepos, { isLoading: isRepoLoad,
+      data: dataRepos }
+   ] = useLazyGetUserReposQuery();
+
    useEffect(() => {
       setDropdown(debounced.length > 3 && data?.length! > 0)
    }, [debounced, data]);
 
-
+   const clickDroprownItem = (username: string) => {
+      fetchRepos(username);
+      setDropdown(false);
+   }
    return (
       <div className='flex justify-center pt-10 mx-auto 
       h-screen w-screen'>
@@ -32,21 +41,27 @@ export function Home() {
             />
             {dropdown &&
                <ul
-               className='list-none absolute top-[42px] left-0 
+                  className='list-none absolute top-[42px] left-0 
                right-0 max-h-[200px] shadow-md bg-white
                overflow-y-scroll'>
-
-               {isLoading && <p className='text-center'>Loading...</p>}
-
-               {data?.map(user => (
-                  <li
-                     className='py-2 px-4 hover:bg-gray-500 hover:text-red
-                  transition-colors cursor-pointer'
-                     key={user.id}>
-                     {user.login}
-                  </li>
-               ))}
-            </ul>}
+                  {isLoading && <p className='text-center'>Loading...</p>}
+                  {data?.map(user => (
+                     <li
+                        className='py-2 px-4 hover:bg-gray-500 hover:text-red
+                        transition-colors cursor-pointer'
+                        key={user.id}
+                        onClick={() => clickDroprownItem(user.login)}>
+                        {user.login}
+                     </li>
+                  ))}
+               </ul>}
+            <div className="container">
+               {isRepoLoad && <p className='text-center'>
+                  Repos are loading...</p>}
+               { dataRepos?.map(repo => 
+                  <ReposCard repo={repo} key={repo.id} />
+                  )}
+            </div>
          </div>
       </div>
    )
